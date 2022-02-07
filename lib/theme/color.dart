21c446a642f5f2a8f54aad34158e025f39810af4
meenkuna <79505706+meenkuna@users.noreply.github.com>
2022-02-07 09:24:29 +0700

    flutter upgrade

---
+++ lib/theme/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColor = Color(0xFFF5F5F5);
+Color primaryColor = Color(0xFF00A6ED);
+Color primaryColorDark = Color(0xFF00A6ED);
+Color primaryColorLight = Color(0xFF263238);
+
+final colorTheme = ThemeData(primarySwatch: Colors.brown[50]);
